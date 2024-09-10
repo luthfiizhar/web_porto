bbd5aa4ba0bb2efa9313d5189b0ae1159d279564
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2024-09-10 22:13:29 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/web_porto",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
+  // basePath: "/web_porto",
+  // output: "export", // <=== enables static exports
+  // reactStrictMode: true,
+  // images: {
+  //   unoptimized: false,
+  // },
 };
 
 export default nextConfig;
