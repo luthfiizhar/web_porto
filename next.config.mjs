34e68f4f783fdb6fca2f9ed8d3cf0e29094dcf02
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2024-09-16 07:19:05 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/web_porto",
+  trailingSlash: true,
+  assetPrefix: "/web_porto/",
   output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
-    loader: "custom",
-    loaderFile: "./loader.js",
+    // loader: "custom",
+    // loaderFile: "./loader.js",
     // remotePatterns: {
     //   protocol: "https",
     //   hostname: "luthfiizhar.github.io/web_porto/",
